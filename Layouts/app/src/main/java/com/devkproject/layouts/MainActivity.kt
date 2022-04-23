bc07221d4e113b157a683e3a62896d17826483e6
devmk1090 <devmk1090@gmail.com>
2022-04-24 02:27:59 +0900

    Modifier(수정자) - Composable의 크기, 레이아웃, 동작, 모양 변경 - 접근성 라벨과 같은 정보 추가 - 사용자 입력 처리 - 클릭, 스크롤, 드래그, 확대/축소등 높은 수준의 상호작용 추가
    * Modifier를 호출하는 순서가 중요하다 !

--- Layouts/app/src/main/java/com/devkproject/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/devkproject/layouts/MainActivity.kt
@@ -3,41 +3,66 @@ package com.devkproject.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.devkproject.layouts.ui.theme.LayoutsTheme
+import androidx.compose.ui.unit.dp
+import com.devkproject.layouts.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LayoutsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            LayoutsCodelabTheme {
+                PhotographerCard()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row (modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick = { /* Ignoring onClick */ })
+        .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column (
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    LayoutsTheme {
-        Greeting("Android")
+fun PhotographerCardPreview() {
+    LayoutsCodelabTheme {
+        PhotographerCard()
     }
 }
--- Layouts/app/src/main/java/com/devkproject/layouts/ui/theme/Theme.kt
+++ Layouts/app/src/main/java/com/devkproject/layouts/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun LayoutsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun LayoutsCodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
