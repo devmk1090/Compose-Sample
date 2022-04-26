7e43746ea19f87cfcbd8a5d7f05aff248e0feb40
devmk1090 <devmk1090@gmail.com>
2022-04-27 02:34:15 +0900

    레이아웃 수정자 - layout 수정자를 사용하여 요소를 측정하고 배치하는 방법을 수동으로 제어한다. layout 수정자를 사용하면 람다 매개변수 두 개를 얻을 수 있다. measurable: 측정하고 배치할 하위 요소 constraints: 하위 요소의 너비와 높이 최솟값과 최댓값 * 컴포트 UI는 다중 측정을 허용하지 않는다. 즉 자식을 두번 이상 측정할 수 없고, 단 한번만 측정하는 것을 의미한다.

--- Layouts/app/src/main/java/com/devkproject/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/devkproject/layouts/MainActivity.kt
@@ -16,8 +16,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.devkproject.layouts.ui.theme.LayoutsCodelabTheme
@@ -28,9 +33,89 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                ScrollingList()
+
             }
         }
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        //Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        //Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            //Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    //custom layout attributes
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        //Don't constrain child views further, measure them with given constraints
+        //List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        //Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        //Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContent2(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        BodyContent2()
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
@@ -83,14 +168,6 @@ fun ScrollingList() {
                 ImageListItem(it)
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun LayoutsCodelabPreview() {
-    LayoutsCodelabTheme {
-        ScrollingList()
     }
 }
 
