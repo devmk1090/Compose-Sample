4f6ffe7c6ac35b787c729b6befec236236730eb1
devmk1090 <devmk1090@gmail.com>
2022-04-30 03:13:48 +0900

    Intrinsics(내장 기능) - 내장 기능을 사용하면 하위 요소가 실제로 측정되기 전에 하위 요소를 쿼리할 수 있다. 예를 들어 width가 무한대인 Text의 minIntrinsicHeight를 요청하면 텍스트가 한 줄에 그려진 것처럼 Text의 height가 반환된다. - height(IntrinsicSize.Min)는 하위 요소의 크기를 고유한 최소 높이로 강제 지정한다.

--- Layouts/app/src/main/java/com/devkproject/layouts/ConstraintLayout.kt
+++ Layouts/app/src/main/java/com/devkproject/layouts/ConstraintLayout.kt
@@ -1,12 +1,18 @@
 package com.devkproject.layouts
 
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 import com.devkproject.layouts.ui.theme.LayoutsCodelabTheme
 
 @Composable
@@ -40,6 +46,65 @@ fun ConstraintLayoutContent() {
         ) {
             Text("Button 2")
         }
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp) //Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) //Landscape constraints
+        }
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
+            }
+
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin= margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text(
+            "This is a very very very very very very very long text",
+            Modifier.constrainAs(text) {
+                linkTo(guideline, parent.end)
+                width = Dimension.preferredWrapContent.atLeast(100.dp)
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+    LayoutsCodelabTheme {
+        LargeConstraintLayout()
     }
 }
 
