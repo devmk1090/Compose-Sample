0f1e98fa2901b1566eb344212ea2d7b51c351da9
devmk1090 <devmk1090@gmail.com>
2022-05-07 02:31:45 +0900

    Setting Material Theme(기본 셋팅 참고) - Jetpack Compose에서 테마 설정을 구현하는 핵심 요소는 MaterialTheme 컴포저블이다. 이 컴포저블을 Compose 계층 구조에 배치하면 그 안의 모든 구성요소의 색상과 유형, 도형 맞춤설정을 지정할 수 있다. - 스타일을 중앙 집중화하려면 MaterialTheme을 래핑하고 구성하는 자체 컴포저블을 만드는 것이 좋다.
    - Colors, Typography, Shapes 속성을 노출하는 MaterialTheme object를 사용하여 컴포저블에 전달된 매개변수를 검색할 수 있다.
    - Color : 색상 지정을 위한 일반적인 '#dd0d3c' 형식에서 변환하려면 '#'을 '0xff', 즉 Color(0xffdd0d3c)로 대체한다. 여기서 'ff'는 완전한 알파를 의미함.
    - Typography : MaterialTheme에서 허용하는 Typography 객체를 만들어 스케일의 각 시맨틱 스타일에 관한 TextStyle을 지정한다.
    
    - 어두운 테마
    앱에서 어두운 테마를 지원하면 앱이 사용자 기기(Android 10부터 전역 어두운 테마 전환이 가능함)에서 더 잘 통합될 뿐만 아니라 전력 사용량을 줄이고 접근성 요구사항을 지원할 수 있다.

---
+++ Theming/app/src/main/java/com/devkproject/theming/ui/start/Home.kt
@@ -0,0 +1,2 @@
+package com.devkproject.theming.ui.start
+
---
+++ Theming/app/src/main/java/com/devkproject/theming/ui/start/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.devkproject.theming.ui.start
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface {
+
+            }
+        }
+    }
+}
