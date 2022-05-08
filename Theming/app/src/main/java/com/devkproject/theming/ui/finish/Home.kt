8dbbbee4c6405ed0bd1e615a294b7e99c88e427a
devmk1090 <devmk1090@gmail.com>
2022-05-09 00:52:07 +0900

    Material Theme Sample App(사전에 정의한 테마를 기본으로 간단한 샘플 구성) - 텍스트에 여러 스타일을 적용해야 하는 경우 마크업을 적용하는 AnnotatedString 클래스를 사용하면 SpanStyle을 텍스트 범위에 추가할 수 있다.

--- Theming/app/src/main/java/com/devkproject/theming/ui/finish/Home.kt
+++ Theming/app/src/main/java/com/devkproject/theming/ui/finish/Home.kt
@@ -4,23 +4,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Palette
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.devkproject.theming.R
 import com.devkproject.theming.data.Post
 import com.devkproject.theming.data.PostRepo
 import com.devkproject.theming.ui.finish.theme.JetnewsTheme
+import java.util.*
 
 @Composable
 fun Home() {
@@ -41,6 +47,10 @@ fun Home() {
                 }
                 item {
                     Header(stringResource(R.string.popular))
+                }
+                items(posts) { post ->
+                    PostItem(post = post)
+                    Divider(startIndent = 72.dp)
                 }
             }
         }
@@ -118,6 +128,76 @@ fun FeaturedPost(
                 modifier = padding
             )
             Spacer(Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+private fun PostMetadata(
+    post: Post,
+    modifier: Modifier = Modifier
+) {
+    val divider = "  •  "
+    val tagDivider = "  "
+    val text = buildAnnotatedString {
+        append(post.metadata.date)
+        append(divider)
+        append(stringResource(R.string.read_time, post.metadata.readTimeMinutes))
+        append(divider)
+        val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+            background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+        )
+        post.tags.forEachIndexed { index, tag ->
+            if (index != 0) {
+                append(tagDivider)
+            }
+            withStyle(tagStyle) {
+                append(" ${tag.uppercase(Locale.getDefault())} ")
+            }
+        }
+    }
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body2,
+            modifier = modifier
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PostItem(
+    post: Post,
+    modifier: Modifier = Modifier
+) {
+    ListItem(
+        modifier = modifier
+            .clickable { /* todo */ }
+            .padding(vertical = 8.dp),
+        icon = {
+            Image(
+                painter = painterResource(post.imageThumbId),
+                contentDescription = null,
+                modifier = Modifier.clip(shape = MaterialTheme.shapes.small)
+            )
+        },
+        text = {
+            Text(text = post.title)
+        },
+        secondaryText = {
+            PostMetadata(post)
+        }
+    )
+}
+
+@Preview("Post Item")
+@Composable
+private fun PostItemPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    JetnewsTheme {
+        Surface {
+            PostItem(post = post)
         }
     }
 }
