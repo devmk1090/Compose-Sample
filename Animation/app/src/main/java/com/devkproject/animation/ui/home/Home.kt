714c60e36ce3a372d1873f3daf6c72aa482ec54d
devmk1090 <devmk1090@gmail.com>
2022-05-10 01:48:27 +0900

    Initial setup complete

---
+++ Animation/app/src/main/java/com/devkproject/animation/ui/home/Home.kt
@@ -0,0 +1,15 @@
+package com.devkproject.animation.ui.home
+
+import androidx.compose.runtime.Composable
+
+private enum class TabPage {
+    Home, Work
+}
+
+/**
+ * Shows the entire screen.
+ */
+@Composable
+fun Home() {
+
+}
