d20e0a630511a2b5fea98b315d6e590c237b3071
devmk1090 <devmk1090@gmail.com>
2022-05-11 00:50:51 +0900

    Make TabBar and Setting FloatingActionButton

--- Animation/app/src/main/java/com/devkproject/animation/ui/home/Home.kt
+++ Animation/app/src/main/java/com/devkproject/animation/ui/home/Home.kt
@@ -1,6 +1,31 @@
 package com.devkproject.animation.ui.home
 
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.devkproject.animation.R
+import com.devkproject.animation.ui.Green300
+import com.devkproject.animation.ui.Green800
+import com.devkproject.animation.ui.Purple100
+import com.devkproject.animation.ui.Purple700
 
 private enum class TabPage {
     Home, Work
@@ -11,5 +36,182 @@ private enum class TabPage {
  */
 @Composable
 fun Home() {
+    // String resources.
+    val allTasks = stringArrayResource(R.array.tasks)
+    val allTopics = stringArrayResource(R.array.topics).toList()
 
+    // The currently selected tab.
+    var tabPage by remember { mutableStateOf(TabPage.Home) }
+
+    // True if the whether data is currently loading.
+    var weatherLoading by remember { mutableStateOf(false) }
+
+    // Holds all the tasks currently shown on the task list.
+    val tasks = remember { mutableStateListOf(*allTasks) }
+
+    // Holds the topic that is currently expanded to show its body.
+    var expandedTopic by remember { mutableStateOf<String?>(null) }
+
+    // True if the message about the edit feature is shown.
+    var editMessageShown by remember { mutableStateOf(false) }
+
+    val lazyListState = rememberLazyListState()
+
+
+    val backgroundColor = if (tabPage == TabPage.Home) Purple100 else Green300
+
+    // The coroutine scope for event handlers calling suspend functions.
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            HomeTabBar(
+                backgroundColor = backgroundColor,
+                tabPage = tabPage,
+                onTabSelected = { tabPage = it }
+            )
+        },
+        backgroundColor = backgroundColor,
+        floatingActionButton = {
+            {
+
+            }
+        }
+    ) {
+
+    }
+}
+
+/**
+ * Shows the floating action button.
+ *
+ * @param extended Whether the tab should be shown in its expanded state.
+ */
+// AnimatedVisibility is currently an experimental API in Compose Animation.
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun HomeFloatingActionButton(
+    extended: Boolean,
+    onClick: () -> Unit
+) {
+    // Use `FloatingActionButton` rather than `ExtendedFloatingActionButton` for full control on
+    // how it should animate.
+    FloatingActionButton(onClick = onClick) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = null)
+        }
+
+        if (extended) {
+            Text(
+                text = stringResource(R.string.edit),
+                modifier = Modifier
+                    .padding(start = 8.dp, top = 3.dp))
+        }
+    }
+}
+
+/**
+ * Shows the bar that holds 2 tabs.
+ *
+ * @param backgroundColor The background color for the bar.
+ * @param tabPage The [TabPage] that is currently selected.
+ * @param onTabSelected Called when the tab is switched.
+ */
+@Composable
+private fun HomeTabBar(
+    backgroundColor: Color,
+    tabPage: TabPage,
+    onTabSelected: (tabPage: TabPage) -> Unit
+) {
+    TabRow(
+        selectedTabIndex = tabPage.ordinal,
+        backgroundColor = backgroundColor,
+        indicator = { tabPositions ->
+            HomeTabIndicator(tabPositions, tabPage)
+        }
+    ) {
+        HomeTab(
+            icon = Icons.Default.Home,
+            title = stringResource(R.string.home),
+            onClick = { onTabSelected(TabPage.Home) }
+        )
+        HomeTab(
+            icon = Icons.Default.AccountBox,
+            title = stringResource(R.string.work),
+            onClick = { onTabSelected(TabPage.Work) }
+        )
+    }
+}
+
+/**
+ * Shows an indicator for the tab.
+ *
+ * @param tabPositions The list of [TabPosition]s from a [TabRow].
+ * @param tabPage The [TabPage] that is currently selected.
+ */
+@Composable
+private fun HomeTabIndicator(
+    tabPosition: List<TabPosition>,
+    tabPage: TabPage
+) {
+    val indicatorLeft = tabPosition[tabPage.ordinal].left
+    val indicatorRight = tabPosition[tabPage.ordinal].right
+    val color = if (tabPage == TabPage.Home) Purple700 else Green800
+    Box(
+        androidx.compose.ui.Modifier
+            .fillMaxSize()
+            .wrapContentSize(align = Alignment.BottomStart)
+            .offset(x = indicatorLeft)
+            .width(indicatorRight - indicatorLeft)
+            .padding(4.dp)
+            .fillMaxSize()
+            .border(
+                BorderStroke(2.dp, color),
+                RoundedCornerShape(4.dp)
+            )
+    )
+}
+
+/**
+ * Shows a tab.
+ *
+ * @param icon The icon to be shown on this tab.
+ * @param title The title to be shown on this tab.
+ * @param onClick Called when this tab is clicked.
+ * @param modifier The [Modifier].
+ */
+@Composable
+private fun HomeTab(
+    icon: ImageVector,
+    title: String,
+    onClick: () -> Unit,
+    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier
+) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Spacer(modifier = androidx.compose.ui.Modifier.width(16.dp))
+        Text(text = title)
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewHomeTabBar() {
+    HomeTabBar(
+        backgroundColor = Purple100,
+        tabPage = TabPage.Home,
+        onTabSelected = {}
+    )
 }
