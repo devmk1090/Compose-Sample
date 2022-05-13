0b9ce1982724e3d3bd03132e582f328464d3e456
devmk1090 <devmk1090@gmail.com>
2022-05-14 02:31:04 +0900

    Add finished code(50%)

--- Animation/finished/src/main/java/com/devkproject/finished/ui/home/Home.kt
+++ Animation/finished/src/main/java/com/devkproject/finished/ui/home/Home.kt
@@ -1,7 +1,348 @@
 package com.devkproject.finished.ui.home
 
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import com.devkproject.finished.R
+import com.devkproject.finished.ui.Green300
+import com.devkproject.finished.ui.Green800
+import com.devkproject.finished.ui.Purple100
+import com.devkproject.finished.ui.Purple700
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+private enum class TabPage {
+    Home, Work
+}
+
+/**
+ * Shows the entire screen.
+ */
 @Composable
 fun Home() {
+    // String resources.
+    val allTasks = stringArrayResource(R.array.tasks)
+    val allTopics = stringArrayResource(R.array.topics).toList()
+
+    // The currently selected tab.
+    var tabPage by remember { mutableStateOf(TabPage.Home) }
+
+    // True if the whether data is currently loading.
+    var weatherLoading by remember { mutableStateOf(false) }
+
+    // Holds all the tasks currently shown on the task list.
+    val tasks = remember { mutableStateListOf(*allTasks) }
+
+    // Holds the topic that is currently expanded to show its body.
+    var expandedTopic by remember { mutableStateOf<String?>(null) }
+
+    // True if the message about the edit feature is shown.
+    var editMessageShown by remember { mutableStateOf(false) }
+
+    // Simulates loading weather data. This takes 3 seconds.
+    suspend fun loadWeather() {
+        if (!weatherLoading) {
+            weatherLoading = true
+            delay(3000L)
+            weatherLoading = false
+        }
+    }
+
+    // Shows the message about edit feature.
+    suspend fun showEditMessage() {
+        if (!editMessageShown) {
+            editMessageShown = true
+            delay(3000L)
+            editMessageShown = false
+        }
+    }
+
+    // Load the weather at the initial composition
+    LaunchedEffect(Unit) {
+        loadWeather()
+    }
+
+    val lazyListState = rememberLazyListState()
+
+    // The background color. The value is changed by the current tab.
+    val backgroundColor by animateColorAsState(if (tabPage == TabPage.Home) Purple100 else Green300)
+
+    // The coroutine scope for event handlers calling suspend functions.
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            HomeTabBar(
+                backgroundColor = backgroundColor,
+                tabPage = tabPage,
+                onTabSelected = { tabPage = it }
+            )
+        },
+        backgroundColor = backgroundColor,
+        floatingActionButton = {
+            HomeFloatingActionButton(
+                extended = lazyListState.isScrollingUp(),
+                onClick = {
+                    coroutineScope.launch {
+                        showEditMessage()
+                    }
+                }
+            )
+        }
+    ) {
+
+    }
+}
+
+/**
+ * Shows the floating action button.
+ *
+ * @param extended Whether the tab should be shown in its expanded state.
+ */
+// AnimatedVisibility is currently an experimental API in Compose Animation.
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun HomeFloatingActionButton(
+    extended: Boolean,
+    onClick: () -> Unit
+) {
+    // Use `FloatingActionButton` rather than `ExtendedFloatingActionButton` for full control on
+    // how it should animate.
+    FloatingActionButton(onClick = onClick) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = null
+            )
+            // Toggle the visibility of the content with animation.
+            AnimatedVisibility(visible = extended) {
+                Text(
+                    text = stringResource(R.string.edit),
+                    modifier = Modifier
+                        .padding(start = 8.dp, top = 3.dp))
+            }
+        }
+    }
+}
+
+/**
+ * Shows a message that the edit feature is not available.
+ */
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun EditMessage(shown: Boolean) {
+    AnimatedVisibility(
+        visible = shown,
+        enter = slideInVertically(
+            // Enters by sliding in from offset -fullHeight to 0.
+            initialOffsetY = { fullHeight -> -fullHeight },
+            animationSpec = tween(durationMillis = 150, easing = LinearOutSlowInEasing)
+        ),
+        exit = slideOutVertically(
+            // Exits by sliding out from offset 0 to -fullHeight.
+            targetOffsetY = { fullHeight -> -fullHeight },
+            animationSpec = tween(durationMillis = 250, easing = FastOutLinearInEasing)
+        )
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colors.secondary,
+            elevation = 4.dp
+        ) {
+            Text(
+                text = stringResource(R.string.edit_message),
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+
+/**
+ * Returns whether the lazy list is currently scrolling up.
+ */
+@Composable
+private fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
+}
+
+/**
+ * Shows the header label.
+ *
+ * @param title The title to be shown.
+ */
+@Composable
+private fun Header(
+    title: String
+) {
+    Text(
+        text = title,
+        modifier = Modifier.semantics { heading() },
+        style = MaterialTheme.typography.h5
+    )
+}
+
+/**
+ * Shows the bar that holds 2 tabs.
+ *
+ * @param backgroundColor The background color for the bar.
+ * @param tabPage The [TabPage] that is currently selected.
+ * @param onTabSelected Called when the tab is switched.
+ */
+@Composable
+private fun HomeTabBar(
+    backgroundColor: Color,
+    tabPage: TabPage,
+    onTabSelected: (tabPage: TabPage) -> Unit
+) {
+    TabRow(
+        selectedTabIndex = tabPage.ordinal,
+        backgroundColor = backgroundColor,
+        indicator = { tabPositions ->
+            HomeTabIndicator(tabPositions, tabPage)
+        }
+    ) {
+        HomeTab(
+            icon = Icons.Default.Home,
+            title = stringResource(R.string.home),
+            onClick = { onTabSelected(TabPage.Home) }
+        )
+        HomeTab(
+            icon = Icons.Default.AccountBox,
+            title = stringResource(R.string.work),
+            onClick = { onTabSelected(TabPage.Work) }
+        )
+    }
+}
+
+/**
+ * Shows an indicator for the tab.
+ *
+ * @param tabPositions The list of [TabPosition]s from a [TabRow].
+ * @param tabPage The [TabPage] that is currently selected.
+ */
+@Composable
+private fun HomeTabIndicator(
+    tabPositions: List<TabPosition>,
+    tabPage: TabPage
+) {
+    val transition = updateTransition(
+        tabPage,
+        label = "Tab indicator"
+    )
+    val indicatorLeft by transition.animateDp(
+        transitionSpec = {
+            if (TabPage.Home isTransitioningTo TabPage.Work) {
+                // Indicator moves to the right.
+                // Low stiffness spring for the left edge so it moves slower than the right edge.
+                spring(stiffness = Spring.StiffnessVeryLow)
+            } else {
+                // Indicator moves to the left.
+                // Medium stiffness spring for the left edge so it moves faster than the right edge.
+                spring(stiffness = Spring.StiffnessMedium)
+            }
+        },
+        label = "Indicator left"
+    ) { page ->
+        tabPositions[page.ordinal].left
+    }
+    val indicatorRight by transition.animateDp(
+        transitionSpec = {
+            if (TabPage.Home isTransitioningTo TabPage.Work) {
+                // Indicator moves to the right
+                // Medium stiffness spring for the right edge so it moves faster than the left edge.
+                spring(stiffness = Spring.StiffnessMedium)
+            } else {
+                // Indicator moves to the left.
+                // Low stiffness spring for the right edge so it moves slower than the left edge.
+                spring(stiffness = Spring.StiffnessVeryLow)
+            }
+        },
+        label = "Indicator right"
+    ) { page ->
+        tabPositions[page.ordinal].right
+    }
+    val color by transition.animateColor(
+        label = "Border color"
+    ) { page ->
+        if (page == TabPage.Home) Purple700 else Green800
+    }
+    Box(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize(align = Alignment.BottomStart)
+            .offset(x = indicatorLeft)
+            .width(indicatorRight - indicatorLeft)
+            .padding(4.dp)
+            .fillMaxSize()
+            .border(
+                BorderStroke(2.dp, color),
+                RoundedCornerShape(4.dp)
+            )
+    )
+}
+
+/**
+ * Shows a tab.
+ *
+ * @param icon The icon to be shown on this tab.
+ * @param title The title to be shown on this tab.
+ * @param onClick Called when this tab is clicked.
+ * @param modifier The [Modifier].
+ */
+@Composable
+private fun HomeTab(
+    icon: ImageVector,
+    title: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = title)
+    }
 }
