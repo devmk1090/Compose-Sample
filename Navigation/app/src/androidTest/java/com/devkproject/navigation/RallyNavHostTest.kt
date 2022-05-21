e19b4abca8b8d63de3ba140c4001b503de34862e
devmk1090 <devmk1090@gmail.com>
2022-05-21 15:32:43 +0900

    탐색 테스트 테스트 함수를 만듭니다. 공개되고 @Test 주석이 달려야 합니다. composeTestRule의 setContent를 사용하면 됩니다. @Before 주석이 달린 함수로 setContent를 추출하여 코드를 깔끔하게 유지할 수 있다.

--- Navigation/app/src/androidTest/java/com/devkproject/navigation/RallyNavHostTest.kt
+++ Navigation/app/src/androidTest/java/com/devkproject/navigation/RallyNavHostTest.kt
@@ -4,8 +4,13 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Rule
@@ -37,6 +42,30 @@ class RallyNavHostTest {
         composeTestRule
             .onNodeWithContentDescription("All Accounts")
             .performClick()
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_navigateToBills_viaUI() {
+        //When click on "All Bills"
+        composeTestRule.onNodeWithContentDescription("All Bills").apply {
+            performScrollTo()
+            performClick()
+        }
+        //Then the route is "Bills"
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "Bills")
+    }
+
+    @Test
+    fun rallyNavHost_navigateToAllAccounts_callingNavigate() {
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                navController.navigate(RallyScreen.Accounts.name)
+            }
+        }
         composeTestRule
             .onNodeWithContentDescription("Accounts Screen")
             .assertIsDisplayed()
