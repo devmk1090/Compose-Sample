5e19588f4dc0b8948b53f14b6abb9f10b96e4991
devmk1090 <devmk1090@gmail.com>
2022-05-31 03:03:46 +0900

    Migration Sunflower(25%)

--- Migration/app/src/main/java/com/devkproject/migration/plantdetail/PlantDetailFragment.kt
+++ Migration/app/src/main/java/com/devkproject/migration/plantdetail/PlantDetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -87,6 +88,12 @@ class PlantDetailFragment : Fragment() {
                         true
                     }
                     else -> false
+                }
+            }
+            composeView.setContent {
+                //Compose world!
+                MaterialTheme {
+                    PlantDetailDescription()
                 }
             }
         }
