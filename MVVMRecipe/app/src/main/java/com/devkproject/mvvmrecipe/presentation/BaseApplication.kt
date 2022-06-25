e621d9294696a7914abaaad387599d9abac3e4cf
devmk1090 <devmk1090@gmail.com>
2022-06-26 03:16:40 +0900

    Set theme and res file

---
+++ MVVMRecipe/app/src/main/java/com/devkproject/mvvmrecipe/presentation/BaseApplication.kt
@@ -0,0 +1,16 @@
+package com.devkproject.mvvmrecipe.presentation
+
+import android.app.Application
+import androidx.compose.runtime.mutableStateOf
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+    // should be saved in data store
+    val isDark = mutableStateOf(false)
+
+    fun toggleLightTheme(){
+        isDark.value = !isDark.value
+    }
+}
--- MVVMRecipe/app/src/main/java/com/devkproject/mvvmrecipe/presentation/theme/Shape.kt
+++ MVVMRecipe/app/src/main/java/com/devkproject/mvvmrecipe/presentation/theme/Shape.kt
@@ -1,11 +1,11 @@
-package com.devkproject.mvvmrecipe.ui.theme
+package com.devkproject.mvvmrecipe.presentation.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
+val AppShapes = Shapes(
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    medium = RoundedCornerShape(6.dp),
+    large = RoundedCornerShape(8.dp)
 )
