0ce7a109e3d69039e463ddc6bea2bfd3493b6456
devmk1090 <devmk1090@gmail.com>
2022-07-12 01:54:43 +0900

    Delete AndroidManifest's fullBackupContent and Setting Navigation

--- Survey/app/src/main/java/com/devkproject/survey/MainActivity.kt
+++ Survey/app/src/main/java/com/devkproject/survey/MainActivity.kt
@@ -3,10 +3,16 @@ package com.devkproject.survey
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
     }
 }
--- Survey/app/src/main/java/com/devkproject/survey/welcome/WelcomeScreen.kt
+++ Survey/app/src/main/java/com/devkproject/survey/welcome/WelcomeScreen.kt
@@ -1,15 +1,23 @@
 package com.devkproject.survey.welcome
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.devkproject.survey.R
 import com.devkproject.survey.theme.JetsurveyTheme
 import com.devkproject.survey.util.supportWideScreen
 
@@ -33,8 +41,76 @@ fun WelcomeScreen(onEvent: (WelcomeEvent) -> Unit) {
                     .weight(1f, fill = showBranding)
                     .animateContentSize()
             )
+
+            AnimatedVisibility(
+                showBranding,
+                Modifier.fillMaxWidth()
+            ) {
+                Branding()
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .weight(1f, fill = showBranding)
+                    .animateContentSize()
+            )
+
+            SignInCreateAccount(
+                onEvent = onEvent,
+                onFocusChange = { focused -> showBranding = !focused },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+            )
         }
     }
+}
+
+@Composable
+private fun Branding(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.wrapContentHeight(align = Alignment.CenterVertically)
+    ) {
+        Logo(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(horizontal = 76.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.app_tagline),
+            style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun Logo(
+    modifier: Modifier = Modifier,
+    lightTheme: Boolean = MaterialTheme.colors.isLight
+) {
+    val assetId = if (lightTheme) {
+        R.drawable.ic_logo_light
+    } else {
+        R.drawable.ic_logo_dark
+    }
+    Image(
+        painter = painterResource(id = assetId),
+        modifier = modifier,
+        contentDescription = null
+    )
+}
+
+@Composable
+private fun SignInCreateAccount(
+    onEvent: (WelcomeEvent) -> Unit,
+    onFocusChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val emailState = remember { EmailState() }
 }
 
 @Preview(name = "Welcome light theme")
