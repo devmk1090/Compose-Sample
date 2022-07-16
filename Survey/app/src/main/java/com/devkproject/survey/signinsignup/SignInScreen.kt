12f5fb0fed3d5d1f3195d3cf32910e3dd8f6a28f
devmk1090 <devmk1090@gmail.com>
2022-07-17 02:57:18 +0900

    Completed SignInScreen logic

--- Survey/app/src/main/java/com/devkproject/survey/signinsignup/SignInScreen.kt
+++ Survey/app/src/main/java/com/devkproject/survey/signinsignup/SignInScreen.kt
@@ -1,7 +1,22 @@
 package com.devkproject.survey.signinsignup
 
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.devkproject.survey.R
+import com.devkproject.survey.theme.JetsurveyTheme
+import com.devkproject.survey.theme.snackbarAction
+import com.devkproject.survey.util.supportWideScreen
+import kotlinx.coroutines.launch
 
 sealed class SignInEvent {
     data class SignIn(val email: String, val password: String) : SignInEvent()
@@ -14,4 +29,132 @@ sealed class SignInEvent {
 @Composable
 fun SignIn(onNavigationEvent: (SignInEvent) -> Unit) {
 
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    val snackbarErrorText = stringResource(id = R.string.feature_not_available)
+    val snackbarActionLabel = stringResource(id = R.string.dismiss)
+
+    Scaffold(
+        topBar = {
+            SignInSignUpTopAppBar(
+                topAppBarText = stringResource(id = R.string.sign_in),
+                onBackPressed = { onNavigationEvent(SignInEvent.NavigateBack) }
+            )
+        },
+        content = { contentPadding ->
+            SignInSignUpScreen(
+                modifier = Modifier.supportWideScreen(),
+                contentPadding = contentPadding,
+                onSignedInAsGuest = { onNavigationEvent(SignInEvent.SignInAsGuest) }
+            ) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    SignInContent(
+                        onSignInSubmitted = { email, password ->
+                            onNavigationEvent(SignInEvent.SignIn(email, password))
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextButton(
+                        onClick = {
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    message = snackbarErrorText,
+                                    actionLabel = snackbarActionLabel
+                                )
+                            }
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = stringResource(id = R.string.forgot_password))
+                    }
+                }
+            }
+        }
+    )
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        ErrorSnackbar(
+            snackbarHostState = snackbarHostState,
+            onDismiss = { snackbarHostState.currentSnackbarData?.dismiss() },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun SignInContent(
+    onSignInSubmitted: (email: String, password: String) -> Unit,
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        val focusRequester = remember { FocusRequester() }
+        val emailState = remember { EmailState() }
+        Email(emailState, onImeAction = { focusRequester.requestFocus() })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        val passwordState = remember { PasswordState() }
+        Password(
+            label = stringResource(id = R.string.password),
+            passwordState = passwordState,
+            modifier = Modifier.focusRequester(focusRequester),
+            onImeAction = { onSignInSubmitted(emailState.text, passwordState.text) }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { onSignInSubmitted(emailState.text, passwordState.text) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            enabled = emailState.isValid && passwordState.isValid
+        ) {
+            Text(
+                text = stringResource(id = R.string.sign_in)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ErrorSnackbar(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = { }
+) {
+    SnackbarHost(
+        hostState = snackbarHostState,
+        snackbar = { data ->
+            Snackbar(
+                modifier = Modifier.padding(16.dp),
+                content = {
+                    Text(
+                        text = data.message,
+                        style = MaterialTheme.typography.body2
+                    )
+                },
+                action = {
+                    data.actionLabel?.let {
+                        TextButton(onClick = onDismiss) {
+                            Text(
+                                text = stringResource(id = R.string.dismiss),
+                                color = MaterialTheme.colors.snackbarAction
+                            )
+                        }
+                    }
+                }
+            )
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight(Alignment.Bottom)
+    )
+}
+
+@Preview(name = "Sign in light theme")
+@Composable
+fun SignInPreview() {
+    JetsurveyTheme {
+        SignIn {}
+    }
 }
