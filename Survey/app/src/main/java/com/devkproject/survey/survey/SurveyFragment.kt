198d92bc8beb7f302102cd58ceadd4dce7b48960
devmk1090 <devmk1090@gmail.com>
2022-07-17 23:58:04 +0900

    Prepare Survey logic

---
+++ Survey/app/src/main/java/com/devkproject/survey/survey/SurveyFragment.kt
@@ -0,0 +1,18 @@
+package com.devkproject.survey.survey
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SurveyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ Survey/app/src/main/java/com/devkproject/survey/survey/SurveyViewModel.kt
@@ -0,0 +1,9 @@
+package com.devkproject.survey.survey
+
+import androidx.lifecycle.ViewModel
+
+const val simpleDataFormatPattern = "EEE, MMM d"
+
+class SurveyViewModel : ViewModel() {
+
+}
