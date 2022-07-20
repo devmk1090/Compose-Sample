65f8beb4406107b1aa15662b399526d9d1a77fba
devmk1090 <devmk1090@gmail.com>
2022-07-21 00:55:37 +0900

    Complete PhotoUriManager and SurceyState

---
+++ Survey/app/src/main/java/com/devkproject/survey/survey/PhotoUriManager.kt
@@ -0,0 +1,34 @@
+package com.devkproject.survey.survey
+
+import android.content.ContentValues
+import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+
+/**
+ * Manages the creation of photo Uris. The Uri is used to store the photos taken with camera.
+ */
+class PhotoUriManager(private val appContext: Context) {
+
+    private val photoCollection by lazy {
+        if (Build.VERSION.SDK_INT > 28) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+    }
+
+    private val resolver by lazy { appContext.contentResolver }
+
+    fun buildNewUri() = resolver.insert(photoCollection, buildPhotoDetails())
+
+    private fun buildPhotoDetails() = ContentValues().apply {
+        put(MediaStore.Images.Media.DISPLAY_NAME, generateFilename())
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+    }
+
+    /**
+     * Create a unique file name based on the time the photo is taken
+     */
+    private fun generateFilename() = "selfie-${System.currentTimeMillis()}.jpg"
+}
---
+++ Survey/app/src/main/java/com/devkproject/survey/survey/SurveyState.kt
@@ -0,0 +1,33 @@
+package com.devkproject.survey.survey
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+@Stable
+class QuestionState(
+    val question: Question,
+    val questionIndex: Int,
+    val totalQuestionsCount: Int,
+    val showPrevious: Boolean,
+    val showDone: Boolean
+) {
+    var enableNext by mutableStateOf(false)
+    var answer by mutableStateOf<Answer<*>?>(null)
+}
+
+sealed class SurveyState {
+    data class Questions(
+        @StringRes val surveyTitle: Int,
+        val questionsState: List<QuestionState>
+    ): SurveyState() {
+        var currentQuestionIndex by mutableStateOf(0)
+    }
+
+    data class Result(
+        @StringRes val surveyTitle: Int,
+        val surveyResult: SurveyResult
+    ): SurveyState()
+}
