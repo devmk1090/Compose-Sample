66a1ab4611ef7df8272c72d8e51ae7da6fd528a9
devmk1090 <devmk1090@gmail.com>
2022-07-22 01:57:01 +0900

    Making SurveyFragment base code and Ready to code SurveyViewModel

--- Survey/app/src/main/java/com/devkproject/survey/survey/SurveyFragment.kt
+++ Survey/app/src/main/java/com/devkproject/survey/survey/SurveyFragment.kt
@@ -4,15 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts.TakePicture
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.devkproject.survey.R
+import com.devkproject.survey.theme.JetsurveyTheme
 
 class SurveyFragment : Fragment() {
+
+    private val viewModel: SurveyViewModel by viewModels {
+        SurveyViewModelFactory(PhotoUriManager(requireContext().applicationContext))
+    }
+
+    private val takePicture = registerForActivityResult(TakePicture()) { photoSaved ->
+        if (photoSaved) {
+            viewModel.onImageSaved()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return ComposeView(requireContext()).apply {
+            // In order for savedState to work, the same ID needs to be used for all instances.
+            id = R.id.sign_in_fragment
+
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            setContent {
+                JetsurveyTheme {
+                    v
+                }
+            }
+        }
     }
 }
--- Survey/app/src/main/java/com/devkproject/survey/survey/SurveyViewModel.kt
+++ Survey/app/src/main/java/com/devkproject/survey/survey/SurveyViewModel.kt
@@ -1,11 +1,94 @@
 package com.devkproject.survey.survey
 
-import androidx.lifecycle.ViewModel
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
 
 const val simpleDataFormatPattern = "EEE, MMM d"
 
 class SurveyViewModel(
-
+    private val surveyRepository: SurveyRepository,
+    private val photoUriManager: PhotoUriManager
 ) : ViewModel() {
 
+    private val _uiState = MutableLiveData<SurveyState>()
+    val uiState: LiveData<SurveyState>
+        get() = _uiState
+
+    var askForPermissions by mutableStateOf(true)
+        private set
+
+    private lateinit var surveyInitialState: SurveyState
+
+    // Uri used to save photos taken with the camera
+    private var uri: Uri? = null
+
+    init {
+        viewModelScope.launch {
+            val survey = surveyRepository.getSurvey()
+
+            //Create the default questions state based on the survey questions
+            val questions: List<QuestionState> = survey.questions.mapIndexed { index, question ->
+                val showPrevious = index > 0
+                val showDone = index == survey.questions.size - 1
+                QuestionState(
+                    question = question,
+                    questionIndex = index,
+                    totalQuestionsCount = survey.questions.size,
+                    showPrevious = showPrevious,
+                    showDone = showDone
+                )
+            }
+            surveyInitialState = SurveyState.Questions(survey.title, questions)
+            _uiState.value = surveyInitialState
+        }
+    }
+
+    fun computeResult(surveyQuestions: SurveyState.Questions) {
+        val answers = surveyQuestions.questionsState.mapNotNull { it.answer }
+        val result = surveyRepository.getSurveyResult(answers)
+        _uiState.value = SurveyState.Result(surveyQuestions.surveyTitle, result)
+    }
+
+    fun onDatePicked(questionId: Int, pickerSelection: Long) {
+        updateStateWithActionResult(
+            questionId,
+            SurveyActionResult.Date(pickerSelection)
+        )
+    }
+
+    fun getCurrentDate(questionId: Int): Long {
+        return getSelectedDate(questionId)
+    }
+
+    fun getUriToSaveImage(): Uri? {
+        uri = photoUriManager.buildNewUri()
+        return uri
+    }
+
+    fun onImageSaved() {
+        uri?.let { uri ->
+
+        }
+    }
+
+    // TODO: Ideally this should be stored in the database
+    fun doNotAskForPermissions() {
+        askForPermissions = false
+    }
+}
+
+class SurveyViewModelFactory(
+    private val photoUriManager: PhotoUriManager
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SurveyViewModel::class.java)) {
+            return SurveyViewModel(JetpackSurveyRepository, photoUriManager) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
